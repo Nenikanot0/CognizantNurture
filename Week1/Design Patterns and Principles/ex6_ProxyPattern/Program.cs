5742150542bbd075cea160785fae502ae79c9842
Nenikanot0 <meherneharika503@gmail.com>
2026-06-21 14:14:23 +0530

    proxy pattern added

---
+++ Week1/Design Patterns and Principles/ex6_ProxyPattern/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Image image = new ProxyImage("sample.jpg");
+
+        Console.WriteLine("First Display:");
+        image.Display();
+
+        Console.WriteLine();
+
+        Console.WriteLine("Second Display:");
+        image.Display();
+    }
+}
