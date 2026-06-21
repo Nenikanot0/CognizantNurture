c0b89ea0cb2e2f736f321a4ad36d500ad07f1ac0
Nenikanot0 <meherneharika503@gmail.com>
2026-06-21 14:29:57 +0530

    mvc pattern added

---
+++ Week1/Design Patterns and Principles/ex10_MVCPattern/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Student model = new Student(
+            "Nenika",
+            101,
+            "A"
+        );
+
+        StudentView view = new StudentView();
+
+        StudentController controller =
+            new StudentController(model, view);
+
+        controller.UpdateView();
+
+        Console.WriteLine();
+
+        controller.SetStudentName("Jennie");
+        controller.SetStudentGrade("A+");
+
+        Console.WriteLine("After Update:");
+        controller.UpdateView();
+    }
+}
---
+++ Week1/Design Patterns and Principles/ex10_MVCPattern/StudentView.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class StudentView
+{
+    public void DisplayStudentDetails(string name, int id, string grade)
+    {
+        Console.WriteLine("Student Details:");
+        Console.WriteLine($"Name  : {name}");
+        Console.WriteLine($"ID    : {id}");
+        Console.WriteLine($"Grade : {grade}");
+    }
+}
