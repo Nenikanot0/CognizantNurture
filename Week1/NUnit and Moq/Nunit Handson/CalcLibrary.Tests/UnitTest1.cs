e27287cff400cc5e32e51dd41f39c2d43e79ff9f
Nenikanot0 <meherneharika503@gmail.com>
2026-06-30 23:31:43 +0530

    added nunit handson

---
+++ Week1/NUnit and Moq/Nunit Handson/CalcLibrary.Tests/UnitTest1.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using CalcLibrary;
+
+namespace CalcLibrary.Tests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        private SimpleCalculator calculator;
+
+        [SetUp]
+        public void Setup()
+        {
+            calculator = new SimpleCalculator();
+        }
+
+        [TestCase(10, 20, 30)]
+        [TestCase(5, 7, 12)]
+        [TestCase(100, 200, 300)]
+        public void TestAddition(double a, double b, double expected)
+        {
+            double actual = calculator.Addition(a, b);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+            calculator.AllClear();
+        }
+    }
+}
