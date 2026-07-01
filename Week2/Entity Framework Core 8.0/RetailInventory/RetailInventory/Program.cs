ee03428e69bb4eb2222ac815c7f96b4c059162bb
Nenikanot0 <meherneharika503@gmail.com>
2026-07-01 15:05:46 +0530

    added lab 2-5

---
+++ Week2/Entity Framework Core 8.0/RetailInventory/RetailInventory/Program.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RetailInventory;
+
+using var context = new AppDbContext();
+
+var products = await context.Products
+    .Include(p => p.Category)
+    .ToListAsync();
+
+foreach (var product in products)
+{
+    Console.WriteLine(
+        $"{product.Name} - ₹{product.Price} - Category: {product.Category.Name}"
+    );
+}
