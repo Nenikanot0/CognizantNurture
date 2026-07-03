8d896bb407aaa4fdd5b705551b9bb1c2086f25d2
Nenikanot0 <meherneharika503@gmail.com>
2026-07-03 16:05:00 +0530

    added lab 2 web api

---
+++ Week2/Asp .net/FirstWebAPI/FirstWebAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstWebAPI.Controllers
+{
+    [ApiController]
+    //[Route("api/[controller]")]
+    [Route("api/Emp")]
+    public class EmployeeController : ControllerBase
+    {
+        static List<string> employees = new()
+        {
+            "John",
+            "David",
+            "Alice",
+            "Mary"
+        };
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(employees);
+        }
+    }
+}
